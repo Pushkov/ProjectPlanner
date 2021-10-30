40f46dd44556f06b41997389d9c1db8e97835053
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-10-30 21:09:41 +0300

    profiles

--- app/src/main/java/nicomed/tms/projectplanner/boot/Runner.java
+++ app/src/main/java/nicomed/tms/projectplanner/boot/Runner.java
@@ -1,13 +1,32 @@
 package nicomed.tms.projectplanner.boot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
+
+    @Value("${spring.datasource.password:undef}")
+    private String pwd;
+
+//    @Qualifier("customEncryptor")
+//    @Qualifier("polledEncryptor")
+//    @Autowired
+//    private StringEncryptor stringEncryptor;
+
+
     @Override
     public void run(String... args) throws Exception {
 
         System.out.println("run....");
+        System.out.println("PWD: " + pwd);
+//        String enc = stringEncryptor.encrypt(pwd);
+//        System.out.println("e: " + enc);
+//        System.out.println("d: " + stringEncryptor.decrypt(enc));
+//
+//        System.out.println("********************************");
+//        System.out.println("encPWD: " + stringEncryptor.encrypt(pwd));
+
     }
 }
