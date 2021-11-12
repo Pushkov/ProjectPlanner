8f0592f722567a4834013f3a428122c86f89a89c
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-11-12 23:53:28 +0300

    remove links on engineerjavadto from engineer services

--- services/src/main/java/nicomed/tms/projectplanner/services/EngineerService.java
+++ services/src/main/java/nicomed/tms/projectplanner/services/EngineerService.java
@@ -1,17 +1,12 @@
 package nicomed.tms.projectplanner.services;
 
 import nicomed.tms.projectplanner.dto.EngineerDto;
-import nicomed.tms.projectplanner.dto.EngineerJavaDto;
 import nicomed.tms.projectplanner.entity.Engineer;
 import nicomed.tms.projectplanner.repository.specification.filter.EngineerFilter;
 
 import java.util.List;
 
 public interface EngineerService extends CrudService<Engineer, Long> {
-
-    EngineerJavaDto findJavaDtoByID(long id);
-
-    List<EngineerJavaDto> findAllJavaDto();
 
     List<EngineerDto> findAllListDto();
 
--- services/src/main/java/nicomed/tms/projectplanner/services/jpa/EngineerJpaServiceImpl.java
+++ services/src/main/java/nicomed/tms/projectplanner/services/jpa/EngineerJpaServiceImpl.java
@@ -2,7 +2,6 @@ package nicomed.tms.projectplanner.services.jpa;
 
 import lombok.RequiredArgsConstructor;
 import nicomed.tms.projectplanner.dto.EngineerDto;
-import nicomed.tms.projectplanner.dto.EngineerJavaDto;
 import nicomed.tms.projectplanner.entity.BaseEntity;
 import nicomed.tms.projectplanner.entity.Engineer;
 import nicomed.tms.projectplanner.mapper.EngineerMapper;
@@ -24,16 +23,6 @@ public class EngineerJpaServiceImpl<T extends BaseEntity<ID>, ID> extends Abstra
     @Override
     public JpaRepository<Engineer, Long> getRepository() {
         return engineerRepository;
-    }
-
-    @Override
-    public EngineerJavaDto findJavaDtoByID(long id) {
-        return mapper.mapToJavaDto(findById(id));
-    }
-
-    @Override
-    public List<EngineerJavaDto> findAllJavaDto() {
-        return mapper.mapToListJavaDto((List<Engineer>) findAll());
     }
 
     @Override
