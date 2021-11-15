51b23a09c478ce702a0a79ab721c281769c299a8
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-11-15 08:32:20 +0300

    fix search by specification (in service level)

--- services/src/main/java/nicomed/tms/projectplanner/services/jpa/AppSearchServiceImpl.java
+++ services/src/main/java/nicomed/tms/projectplanner/services/jpa/AppSearchServiceImpl.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import nicomed.tms.projectplanner.dto.AppSearchWrapperDto;
 import nicomed.tms.projectplanner.dto.DocumentDto;
 import nicomed.tms.projectplanner.dto.EngineerDto;
+import nicomed.tms.projectplanner.repository.specification.filter.DocumentFilter;
+import nicomed.tms.projectplanner.repository.specification.filter.EngineerFilter;
+import nicomed.tms.projectplanner.services.AppSearchService;
 import nicomed.tms.projectplanner.services.DocumentService;
 import nicomed.tms.projectplanner.services.EngineerService;
 import nicomed.tms.projectplanner.services.ProjectService;
@@ -14,15 +17,22 @@ import java.util.List;
 
 @JpaImpl
 @RequiredArgsConstructor
-public class AppSearchServiceImpl {
+public class AppSearchServiceImpl implements AppSearchService {
 
     private final DocumentService documentService;
     private final EngineerService engineerService;
     private final ProjectService projectService;
 
+    @Override
     public AppSearchWrapperDto getSearchResult(String term, Boolean document, Boolean engineer) {
-        List<DocumentDto> documentDtoList = new ArrayList<>();// getDocumentDtoList(documentFilter);
-        List<EngineerDto> engineerDtoList = new ArrayList<>();// getEngineerDtoList(engineerFilter);
+        List<DocumentDto> documentDtoList = new ArrayList<>();
+        if (document) {
+            documentDtoList = documentService.search(DocumentFilter.builder().term(term).build());
+        }
+        List<EngineerDto> engineerDtoList = new ArrayList<>();
+        if (engineer) {
+            engineerDtoList = engineerService.search(EngineerFilter.builder().term(term).build());
+        }
         return AppSearchWrapperDto.builder()
                 .documentDtoList(documentDtoList)
                 .engineerDtoList(engineerDtoList)
