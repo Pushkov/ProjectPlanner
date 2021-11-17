0dbb75bcd843b886295bb64e9aaccfd718c07e00
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-11-17 15:30:19 +0300

    add CORS config to ALL PERMIT

---
+++ app/src/main/java/nicomed/tms/projectplanner/config/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package nicomed.tms.projectplanner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("dev")
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
