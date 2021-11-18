065151dc3fbc787d3c6dc90b5fce0489b64b0ba9
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-11-18 16:12:35 +0300

    add test Exception handler

---
+++ app/src/main/java/nicomed/tms/projectplanner/exceptions/ExceptionHandlerController.java
@@ -0,0 +1,26 @@
+package nicomed.tms.projectplanner.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(value
+            = {NoSuchElementException.class})
+    protected ResponseEntity<Object> noSushElementConflict(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "ERROR: " + ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+}
