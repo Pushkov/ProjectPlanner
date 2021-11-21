4583e6eca57578f3400022b7ae57e8278d232d20
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-11-21 18:06:46 +0300

    parameterized entity listener

--- entity/src/main/java/nicomed/tms/listener/SystemOptionalEntityListener.java
+++ entity/src/main/java/nicomed/tms/listener/SystemOptionalEntityListener.java
@@ -1,37 +1,42 @@
 package nicomed.tms.listener;
 
 import lombok.extern.slf4j.Slf4j;
-import nicomed.tms.system.entity.SystemOptionEntity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.PostPersist;
+import javax.persistence.PostRemove;
 import javax.persistence.PostUpdate;
-import javax.persistence.PreUpdate;
 
 @Slf4j
 @Component
-public class SystemOptionalEntityListener {
-
-    private final AuditLogService auditLogService;
+public class SystemOptionalEntityListener extends BaseLogEntityListener {
 
     public static final String AUDIT_ACTIVITY = "system_option";
 
-    @Autowired
     public SystemOptionalEntityListener(AuditLogService auditLogService) {
-        this.auditLogService = auditLogService;
+        super(auditLogService);
     }
 
+    @Override
+    protected String getLogActivity() {
+        return AUDIT_ACTIVITY;
+    }
 
-    @PreUpdate
-    public void loggingPreUpdateSystemOption(SystemOptionEntity entity) {
-        log.warn("PreUpdate {}", entity.getId());
-        auditLogService.createLogs(AUDIT_ACTIVITY, "pre update " + entity.getId());
-
+    @PostPersist
+    @Override
+    protected void loggingPostPersist(Object entity) {
+        super.loggingPostPersist(entity);
     }
 
     @PostUpdate
-    public void loggingPostUpdateSystemOption(SystemOptionEntity entity) {
-        log.warn("PostUpdate {}", entity.getId());
-        auditLogService.createLogs(AUDIT_ACTIVITY, "post update " + entity);
+    @Override
+    protected void loggingPostUpdate(Object entity) {
+        super.loggingPostUpdate(entity);
+    }
+
+    @PostRemove
+    @Override
+    protected void loggingPostDelete(Object entity) {
+        super.loggingPostDelete(entity);
     }
 }
