be22b4070814400494e9e8d2d7997554d4a8d1bc
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-12-01 12:01:44 +0300

    fix creation ID in VIEW add entity, repo, rest_controller for view

---
+++ app/src/main/java/nicomed/tms/projectplanner/rest/PlanProjectRestController.java
@@ -0,0 +1,34 @@
+package nicomed.tms.projectplanner.rest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nicomed.tms.projectplanner.entity.PlanProject;
+import nicomed.tms.projectplanner.repository.PlanProjectRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/planner/api/v1/planprojects")
+public class PlanProjectRestController {
+
+    private final PlanProjectRepository planProjectRepository;
+
+    @GetMapping
+    public List<PlanProject> findAll() {
+        return planProjectRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PlanProject findById(@PathVariable long id) {
+        return planProjectRepository.findById(id)
+                .orElseThrow();
+    }
+
+
+}
