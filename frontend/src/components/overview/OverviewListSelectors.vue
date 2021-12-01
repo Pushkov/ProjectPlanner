42543a8d0e4920faba50a9b26a32d88933a2bdc6
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-12-01 22:00:56 +0300

    add in OVERVIEW get_years, get_months, get_departmentNames

--- frontend/src/components/overview/OverviewListSelectors.vue
+++ frontend/src/components/overview/OverviewListSelectors.vue
@@ -1,78 +1,83 @@
 <template>
-    <div class="row p-3">
-        <div class="col-sm">
-            <div class="row">
-                <h5 class="mx-3">Year:</h5>
-            </div>
-            <b-form-select
-                    v-model="i1"
-                    :options="[1,2]"
-                    class="row text-secondary border rounded-lg"
-                    value-field="id"
-                    text-field="name"
-                    disabled-field="notEnabled"
-            ></b-form-select>
-        </div>
-        <div class="col-sm">
-            <div class="row">
-                <h5 class="mx-3">Month:</h5>
-            </div>
-            <b-form-select
-                    v-model="currentMonth.value"
-                    :options="months"
-                    class="row text-secondary border rounded-lg"
-                    value-field="value"
-                    text-field="name"
-                    disabled-field="notEnabled"
-            ></b-form-select>
-        </div>
-        <div class="col-sm">
-            <div class="row">
-                <h5 class="mx-3">Department:</h5>
-            </div>
-            <b-form-select
-                    v-model="i1"
-                    :options="[1,2]"
-                    class="row text-secondary border rounded-lg"
-                    value-field="id"
-                    text-field="name"
-                    disabled-field="notEnabled"
-            ></b-form-select>
-        </div>
-
-
+  <div class="row p-3">
+    <div class="col-sm">
+      <div class="row">
+        <h5 class="mx-3">Year:</h5>
+      </div>
+      <b-form-select
+          v-model="currentYear"
+          :options="years"
+          class="row text-secondary border rounded-lg"
+          value-field="value"
+          text-field="value"
+          disabled-field="notEnabled"
+      ></b-form-select>
     </div>
+    <div class="col-sm">
+      <div class="row">
+        <h5 class="mx-3">Month:</h5>
+      </div>
+      <b-form-select
+          v-model="currentMonth"
+          :options="months"
+          class="row text-secondary border rounded-lg"
+          value-field="value"
+          text-field="value"
+          disabled-field="notEnabled"
+      ></b-form-select>
+    </div>
+    <div class="col-sm">
+      <div class="row">
+        <h5 class="mx-3">Department:</h5>
+      </div>
+      <b-form-select
+          v-model="currentDepartment"
+          :options="departments"
+          class="row text-secondary border rounded-lg"
+          value-field="value"
+          text-field="value"
+          disabled-field="notEnabled"
+      ></b-form-select>
+    </div>
+
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "OverviewListSelectors",
-        data() {
-            return {
-                currentMonth: {
-                    value: -1,
-                    name: 't name'
-                },
-                i1: {
-                    id: 1,
-                    name: 't name'
-                },
-                i3: {
-                    id: 1,
-                    name: 't name'
-                },
-            }
-        },
-        props: {
-            months: {
-                type: Array,
-                default:
-                    () => {
-                        return []
-                    }
-            }
-        }
+export default {
+  name: "OverviewListSelectors",
+  data() {
+    return {
+      currentYear: 2021,
+      currentMonth: 11,
+      currentDepartment: 'УПиР'
     }
+  },
+  props: {
+    years: {
+      type: Array,
+      default:
+          () => {
+            return []
+          }
+    },
+    months: {
+      type: Array,
+      default:
+          () => {
+            return []
+          }
+    },
+    departments: {
+      type: Array,
+      default:
+          () => {
+            return []
+          }
+    },
+  }
+}
 
 </script>
 
