17a42d0a574137a50b43099a3a54518661bba284
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-12-09 16:29:44 +0300

    template for PLAN in FRONTEND

--- frontend/src/components/plan/PlansLisRow.vue
+++ frontend/src/components/plan/PlansLisRow.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    {{ plan.year }}-{{ plan.month }}-{{ plan.departmentId }}-{{ plan.departmentName }}
     <template v-if="plan.planPointsDto !== undefined && plan.planPointsDto.length > 0">
       <div v-for="pp of plan.planPointsDto"
            :key="pp.id"
       >
-        {{ pp.id }}-{{ pp.year }}-{{ pp.month }}-{{ pp.departmentName }}-{{ pp.projectDesignation }}-{{ pp.projectName }}
+        **************************** {{ pp.projectDesignation }}-{{ pp.projectName }}
       </div>
     </template>
   </div>
--- frontend/src/components/plan/PlansList.vue
+++ frontend/src/components/plan/PlansList.vue
@@ -1,41 +1,50 @@
 <template>
-  <div>
-    <template v-if="PLANS !== undefined && PLANS.length > 0">
-      <PlansLisRow
-          v-for="(plan, index) of PLANS"
-          :key="index"
-          :plan="plan"
-      />
+    <div>
+        <template v-if="PLANS !== undefined && PLANS.length > 0">
+
+            <div>SELECTORS</div>
+
+            <div v-for="(plan, index) of PLANS"
+                 :key="index"
+                 :plan="plan">
+
+                Year: {{plan.year}}; Month: {{plan.month}}; Department: {{plan.departmentName}}
+                <div>
+                    <PlansLisRow
+                            :plan="plan"
+                    />
+                </div>
+            </div>
 
 
-    </template>
-    <template v-else>
-      ! PLANS LIST IS EMPTY
-    </template>
-  </div>
+        </template>
+        <template v-else>
+            ! PLANS LIST IS EMPTY
+        </template>
+    </div>
 </template>
 
 <script>
-import {mapActions, mapGetters} from "vuex";
-import PlansLisRow from "@/components/plan/PlansLisRow";
+    import {mapActions, mapGetters} from "vuex";
+    import PlansLisRow from "./PlansLisRow";
 
-export default {
-  name: "PlansList",
-  components: {PlansLisRow},
-  computed: {
-    ...mapGetters([
-      'PLANS'
-    ]),
-  },
-  methods: {
-    ...mapActions([
-      'GET_ALL_PLANS'
-    ]),
-  },
-  mounted() {
-    this.GET_ALL_PLANS();
-  }
-}
+    export default {
+        name: "PlansList",
+        components: {PlansLisRow},
+        computed: {
+            ...mapGetters([
+                'PLANS'
+            ]),
+        },
+        methods: {
+            ...mapActions([
+                'GET_ALL_PLANS'
+            ]),
+        },
+        mounted() {
+            this.GET_ALL_PLANS();
+        }
+    }
 </script>
 
 <style scoped>
