55b21bc74284a10e4e3f2f79b5e54f92a4dc61cd
Pushkov Andrey <pushkov.andrey@gmail.com>
2021-12-17 08:59:08 +0300

    add password data for ENGINEER

--- app/src/test/java/nicomed/tms/projectplanner/controller/rest/PasswordEncodingTest.java
+++ app/src/test/java/nicomed/tms/projectplanner/controller/rest/PasswordEncodingTest.java
@@ -12,19 +12,40 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PasswordEncodingTest {
 
     public static final String PASSWORD = "password";
+    public static final String PWD_PUSHKOV = "pan";
+    public static final String PWD_LAPTEV = "lsv";
+    public static final String PWD_KUPREEV = "kmp";
+    public static final String PWD_MELNIKOV = "mvv";
+    public static final String PWD_MULUTINA = "mlr";
+    public static final String PWD_SOROKOVIKOVA = "sve";
+    public static final String PWD_NAGORNY = "nam";
 
     @Test
     void testbcrypt() {
         PasswordEncoder bcrypt = new BCryptPasswordEncoder(8);
-//        System.out.println(bcrypt.encode(PASSWORD));
-//        System.out.println(bcrypt.encode(PASSWORD));
+        System.out.println("Пушков");
+        System.out.println(bcrypt.encode(PWD_PUSHKOV));
+        System.out.println("Лаптев");
+        System.out.println(bcrypt.encode(PWD_LAPTEV));
+        System.out.println("Купреев");
+        System.out.println(bcrypt.encode(PWD_KUPREEV));
+        System.out.println("Мельников");
+        System.out.println(bcrypt.encode(PWD_MELNIKOV));
+        System.out.println("Милютина");
+        System.out.println(bcrypt.encode(PWD_MULUTINA));
+        System.out.println("Сороковикова");
+        System.out.println(bcrypt.encode(PWD_SOROKOVIKOVA));
+        System.out.println("Нагорный");
+        System.out.println(bcrypt.encode(PWD_NAGORNY));
 
-        System.out.println(bcrypt.encode("eng"));
-        System.out.println(bcrypt.encode("nmd"));
-        System.out.println(bcrypt.encode("nko"));
-        System.out.println(bcrypt.encode("zmn"));
-        System.out.println(bcrypt.encode("mn"));
-        System.out.println(bcrypt.encode("adm"));
+//        System.out.println(bcrypt.encode("eng"));
+//        System.out.println(bcrypt.encode("nmd"));
+//        System.out.println(bcrypt.encode("nko"));
+//        System.out.println(bcrypt.encode("zmn"));
+//        System.out.println(bcrypt.encode("mn"));
+//        System.out.println(bcrypt.encode("adm"));
+
+
     }
 
     @Test
